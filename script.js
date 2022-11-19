3c276e17161cf21bf579bbaa4aaa5b82cda343ee
Madhav Dua <madhavdua26@gmail.com>
2022-11-19 22:49:55 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let string=""
+let buttons=document.querySelectorAll('button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        string=document.querySelector('input').value
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='C' || e.target.innerHTML=='CE'){
+            string=""
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='Bsc'){
+            string=string.slice(0,string.length-1)
+            document.querySelector('input').value=string;
+        }
+        else{
+            string=string+e.target.innerHTML;
+            document.querySelector('input').value=string;
+        }
+
+    })
+})
